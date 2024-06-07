c4b15228d46f0db8986c9925e3820009f3141f8f
lugamodder <25221685+lugamodder@users.noreply.github.com>
2024-06-07 23:21:03 +0300

    Update get_gpio.sh

--- get_gpio.sh
+++ get_gpio.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-echo 66 > /sys/class/gpio/export
-echo in > /sys/class/gpio/gpio66/direction
-cat /sys/class/gpio/gpio66/value
-echo 66 > /sys/class/gpio/unexport
+gpioget gpiochip2 2
