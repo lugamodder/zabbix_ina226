011f48c5a559b64f824155e350ad7705b15e6dcc
lugamodder <25221685+lugamodder@users.noreply.github.com>
2024-06-13 13:29:01 +0300

    Update get_data.py
    add multiple measurements

--- get_data.py
+++ get_data.py
@@ -9,16 +9,7 @@ address = 0x40
 max_expected_amps = 20
 log_level = logging.CRITICAL
 shunt_ohms=0.002
-
-def read():
-    data = {
-        "bus_voltage": round(ina.voltage(), 3),
-        "bus_current": round((ina.current() / 1000), 3),
-        "supply_voltage": round(ina.supply_voltage() ,3),
-        "shunt_voltage": round(ina.shunt_voltage(), 3),
-        "power": round((ina.power() / 1000), 3)
-    }
-    return json.dumps(data)
+iterations = 10
 
 if __name__ == "__main__":
     ina = INA226(
@@ -31,12 +22,33 @@ if __name__ == "__main__":
 
     ina.configure()
     #ina.set_low_battery(5)
-    sleep(0.2)
-    ina.wake(3)
-    while 1:
-        if ina.is_conversion_ready():
-            json_result = read()
-            print(json_result)
-            break
+    sleep(0.1)
+    measurements = []
+    bus_voltages = []
+    bus_currents = []
+    supply_voltages = []
+    shunt_voltages = []
+    powers = []
 
+    for _ in range(iterations):  # Perform  measurements
+        ina.wake()
+        while True:
+            if ina.is_conversion_ready():
+                bus_voltages.append(ina.voltage())
+                bus_currents.append(ina.current())
+                supply_voltages.append(ina.supply_voltage())
+                shunt_voltages.append(ina.shunt_voltage())
+                powers.append(ina.power())
+                sleep(0.1)
+                break
 
+ # Calculate average values
+    avg_data = {
+        "bus_voltage": round(sum(bus_voltages) / iterations, 3),
+        "bus_current": round(sum(bus_currents) / iterations /1000, 3),
+        "supply_voltage": round(sum(supply_voltages) / iterations, 3),
+        "shunt_voltage": round(sum(shunt_voltages) / iterations, 3),
+        "power": round(sum(powers) / iterations /1000, 3)
+    }
+
+    print(json.dumps(avg_data))
